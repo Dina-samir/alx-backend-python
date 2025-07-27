ae6ec7b0cf821fabfc7eb6e9e8dae18fe3e89b70
Dina-samir <dina24samir@gmail.com>
2025-07-27 14:07:33 +0300

    .

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -3,6 +3,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(PageNumberPagination):
-    page_size = 20
-    page_size_query_param = 'page_size'
+    page_size = 20  # default page size
+    page_size_query_param = 'page_size'  # allow client to override
     max_page_size = 100
